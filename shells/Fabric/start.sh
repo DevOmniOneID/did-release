11029a585e0d50e3f104b7976fbb1f318aca349a
suhyun-forten-lee <shlee1223@raoncorp.com>
2025-02-11 18:57:16 +0900

    Update start.sh

--- shells/Fabric/start.sh
+++ shells/Fabric/start.sh
@@ -1,3 +1,13 @@
+command="./fabric-samples/bin/peer"
+eval $command
+
+if [ $? -eq 0 ]; then
+echo "installation checked"
+else
+./install-fabric.sh d s b
+sleep 5
+fi
+
 ./fabric-samples/test-network/network.sh up -ca -s couchdb
 ./fabric-samples/test-network/network.sh createChannel -c mychannel
 ./fabric-samples/test-network/network.sh deployCC -c mychannel -ccn opendid -ccp ../../did-fabric-contract/source/did-fabric-contract -ccl go -ccs 1
