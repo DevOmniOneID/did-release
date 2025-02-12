45ebb8c57dc38352b6cf91db68bb8328e90c540e
suhyun-forten-lee <shlee1223@raoncorp.com>
2025-02-12 13:53:20 +0900

    Update start.sh

--- shells/Fabric/start.sh
+++ shells/Fabric/start.sh
@@ -14,6 +14,7 @@ fi
 ${PWD}/fabric-samples/test-network/network.sh down
 sleep 10
 ${PWD}/fabric-samples/test-network/network.sh up -ca -s couchdb
+sleep 10
 ${PWD}/fabric-samples/test-network/network.sh createChannel -c mychannel
 ${PWD}/fabric-samples/test-network/network.sh deployCC -c mychannel -ccn opendid -ccp ../../did-fabric-contract/source/did-fabric-contract -ccl go -ccs 1
 sleep 10
