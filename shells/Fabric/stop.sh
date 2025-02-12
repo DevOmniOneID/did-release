1a5def378c44e0533c1b62c06668c447329042ed
suhyun-forten-lee <shlee1223@raoncorp.com>
2025-02-12 14:58:59 +0900

    Update stop.sh

--- shells/Fabric/stop.sh
+++ shells/Fabric/stop.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-${PWD}/fabric-samples/test-network/network.sh down
-
-#echo "Stopping containers"
-#docker stop $(docker ps -aq --filter label=service=hyperledger-fabric; docker ps -aq --filter name='dev-peer*'; docker ps -aq --filter name=ccaas) | sort | uniq 2>/dev/null || true
+echo "Stopping containers"
+docker stop $(docker ps -aq --filter label=service=hyperledger-fabric; docker ps -aq --filter name='dev-peer*'; docker ps -aq --filter name=ccaas) | sort | uniq 2>/dev/null || true
