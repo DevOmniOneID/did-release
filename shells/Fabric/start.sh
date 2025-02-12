f033f92822993cd56133c251973046d6322ecaa8
suhyun-forten-lee <shlee1223@raoncorp.com>
2025-02-12 17:50:12 +0900

    Update start.sh

--- shells/Fabric/start.sh
+++ shells/Fabric/start.sh
@@ -4,15 +4,15 @@ command="${PWD}/fabric-samples/bin/peer"
 eval $command
 
 if [ $? -eq 0 ]; then
-echo "installation checked"
+	echo "installation checked"
 else
-tar -xvf did-fabric-contract-1.0.0.tar.gz
-tar -xvf fabric-samples-1.4.4.tar.gz
-${PWD}/install-fabric.sh --fabric-version 2.5.11 binary
-${PWD}/install-fabric.sh --fabric-version 2.5.1 docker
-sleep 5
-${PWD}/fabric-samples/test-network/network.sh down
-sleep 5
+	tar -xvf did-fabric-contract-1.0.0.tar.gz
+	tar -xvf fabric-samples-1.4.4.tar.gz
+	${PWD}/install-fabric.sh --fabric-version 2.5.11 binary
+	${PWD}/install-fabric.sh --fabric-version 2.5.1 docker
+	sleep 5
+	${PWD}/fabric-samples/test-network/network.sh down
+	sleep 5
 fi
 
 ${PWD}/fabric-samples/test-network/network.sh up
@@ -24,11 +24,11 @@ command="sh ${PWD}/status.sh"
 eval $command
 
 if [ $? -eq 0 ]; then
-echo "starting checked"
+echo "start success"
 else
-${PWD}/fabric-samples/test-network/network.sh createChannel -c mychannel
-sleep 5
-${PWD}/fabric-samples/test-network/network.sh deployCC -c mychannel -ccn opendid -ccp ${PWD}/did-fabric-contract/source/did-fabric-contract -ccl go -ccs 1
+	${PWD}/fabric-samples/test-network/network.sh createChannel -c mychannel
+	sleep 5
+	${PWD}/fabric-samples/test-network/network.sh deployCC -c mychannel -ccn opendid -ccp ${PWD}/did-fabric-contract/source/did-fabric-contract -ccl go -ccs 1
 fi
 
 cp ${PWD}/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/* ${PWD}/cert/sk
