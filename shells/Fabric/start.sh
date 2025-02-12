a3a53d8108175b31c1fef034e8c0240600a297bc
suhyun-forten-lee <shlee1223@raoncorp.com>
2025-02-12 14:10:07 +0900

    Update start.sh

--- shells/Fabric/start.sh
+++ shells/Fabric/start.sh
@@ -8,16 +8,19 @@ echo "installation checked"
 else
 ${PWD}/install-fabric.sh --fabric-version 2.5.11 binary
 ${PWD}/install-fabric.sh --fabric-version 2.5.1 docker
-sleep 10
+sleep 5
 fi
 
 ${PWD}/fabric-samples/test-network/network.sh down
-sleep 10
+sleep 5
+${PWD}/fabric-samples/test-network/network.sh up
+sleep 5
 ${PWD}/fabric-samples/test-network/network.sh up -ca -s couchdb
-sleep 10
+sleep 5
 ${PWD}/fabric-samples/test-network/network.sh createChannel -c mychannel
+sleep 5
 ${PWD}/fabric-samples/test-network/network.sh deployCC -c mychannel -ccn opendid -ccp ../../did-fabric-contract/source/did-fabric-contract -ccl go -ccs 1
-sleep 10
+sleep 5
 
 cp ${PWD}/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/* ${PWD}/cert/sk
 cp ${PWD}/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/signcerts/cert.pem ${PWD}/cert/cert.pem
