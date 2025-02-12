1219624dda55af535067864181a69653c46618e4
suhyun-forten-lee <shlee1223@raoncorp.com>
2025-02-12 11:15:13 +0900

    chore: update status.sh for it to check a chain code healthy

--- shells/Fabric/status.sh
+++ shells/Fabric/status.sh
@@ -1,1 +1,14 @@
-(docker ps -aq --filter label=service=hyperledger-fabric --filter "status=running"; docker ps -aq --filter name='dev-peer*' --filter "status=running"; docker ps -aq --filter name=ccaas --filter "status=running" | sort | uniq) | wc -l
+cd ./fabric-samples/test-network
+
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=$PWD/../config/
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID=Org1MSP
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+peer chaincode query -C mychannel -n opendid -c '{"Args":["document_getDidDoc","did:open:user","1"]}'
+
+#(docker ps -aq --filter label=service=hyperledger-fabric --filter "status=running"; docker ps -aq --filter name='dev-peer*' --filter "status=running"; docker ps -aq --filter name=ccaas --filter "status=running" | sort | uniq) | wc -l
