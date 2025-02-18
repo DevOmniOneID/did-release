366190c5fbf3a121dfb35cc44af4a0684bb42284
sjkim <sjkim@raoncorp.com>
2025-02-18 16:52:22 +0900

    프로퍼티 적용

--- shells/Fabric/start.sh
+++ shells/Fabric/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+CHANNEL=$1
+CHAINCODE_NAME=$2
+
 command="${PWD}/fabric-samples/bin/peer"
 eval $command
 
@@ -13,17 +16,17 @@ else
         ${PWD}/fabric-samples/test-network/network.sh down
 fi
 
-${PWD}/fabric-samples/test-network/network.sh createChannel -c mychannel
+${PWD}/fabric-samples/test-network/network.sh createChannel -c $CHANNEL
 ${PWD}/fabric-samples/test-network/network.sh up -ca -s couchdb
 sleep 3
 
-command="sh ${PWD}/status.sh"
+command="sh ${PWD}/status.sh" $CHANNEL $CHAINCODE_NAME
 eval $command
 
 if [ $? -eq 0 ]; then
         echo "starting checked"
 else
-        ${PWD}/fabric-samples/test-network/network.sh deployCC -c mychannel -ccn opendid -ccp ${PWD}/did-fabric-contract/source/did-fabric-contract -ccl go -ccs 1
+        ${PWD}/fabric-samples/test-network/network.sh deployCC -c $CHANNEL -ccn $CHAINCODE_NAME -ccp ${PWD}/did-fabric-contract/source/did-fabric-contract -ccl go -ccs 1
 fi
 
 cp ${PWD}/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/* ${PWD}/cert/sk
--- shells/Fabric/status.sh
+++ shells/Fabric/status.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+CHANNEL=$1
+CHAINCODE_NAME=$2
+
 export PATH=${PWD}/fabric-samples/bin:$PATH
 export FABRIC_CFG_PATH=$PWD/fabric-samples/config/
 
@@ -9,6 +12,6 @@ export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/fabric-samples/test-network/organizati
 export CORE_PEER_MSPCONFIGPATH=${PWD}/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 export CORE_PEER_ADDRESS=localhost:7051
 
-peer chaincode query -C mychannel -n opendid -c '{"Args":["document_getDidDoc","did:open:user","1"]}'
+peer chaincode query -C $CHANNEL -n $CHAINCODE_NAME -c '{"Args":["document_getDidDoc","did:open:user","1"]}'
 
 #(docker ps -aq --filter label=service=hyperledger-fabric --filter "status=running"; docker ps -aq --filter name='dev-peer*' --filter "status=running"; docker ps -aq --filter name=ccaas --filter "status=running" | sort | uniq) | wc -l
