84b26bdcda51bbf429f40dbd7f8d7eaee957551b
suhyun-forten-lee <shlee1223@raoncorp.com>
2025-02-27 18:27:12 +0900

    기본 설정 플러그인 적용

--- release-V1.0.0.0/deployment/shells/Fabric/start.sh
+++ release-V1.0.0.0/deployment/shells/Fabric/start.sh
@@ -31,3 +31,64 @@ fi
 
 cp ${PWD}/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/* ${PWD}/cert/sk
 cp ${PWD}/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/signcerts/* ${PWD}/cert/cert.pem
+
+#generate blockchain properties
+cat <<EOF > ${PWD}/blockchain.properties
+fabric.mspId=Org1MSP
+fabric.configFilePath=${PWD}/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/connection-org1.yaml
+fabric.privateKeyFilePath=${PWD}/cert/sk
+fabric.certificateFilePath=${PWD}/cert/cert.pem
+fabric.networkName=$CHANNEL
+fabric.chaincodeName=$CHAINCODE_NAME
+EOF
+
+#apply to application.yml
+JAR_PATHS=(
+    "${PWD}/../../jars/TAS"
+    "${PWD}/../../jars/Issuer"
+    "${PWD}/../../jars/Verifier"
+    "${PWD}/../../jars/CAS"
+    "${PWD}/../../jars/WalletService"
+    "${PWD}/../../jars/API"
+)
+
+BLOCKCHAIN_PATH="${PWD}/blockchain.properties"
+
+for JAR_PATH in "${JAR_PATHS[@]}"; do
+    APP_YML="${JAR_PATH}/application.yml"
+
+    if [ -f "$APP_YML" ]; then
+        echo "Updating: $APP_YML"
+
+        # spring.profiles.active: dev
+        awk '
+        BEGIN { found=0 }
+        /^spring:/ { in_spring=1 }
+        in_spring && /^  profiles:/ { in_profiles=1 }
+        in_profiles && /active:/ { found=1; sub(/active: .*/, "active: dev") }
+        { print }
+        END { if (!found) print "  profiles:\n    active: dev" }
+        ' "$APP_YML" > temp.yml && mv temp.yml "$APP_YML"
+
+        # blockchain.file-path: ${PWD}/blockchain.properties
+        awk -v bcpath="$BLOCKCHAIN_PATH" '
+        BEGIN { found=0 }
+        /^blockchain:/ { in_blockchain=1 }
+        in_blockchain && /file-path:/ { found=1; sub(/file-path:.*/, "file-path: " bcpath) }
+        { print }
+        END { if (!found) print "blockchain:\n  file-path: " bcpath }
+        ' "$APP_YML" > temp.yml && mv temp.yml "$APP_YML"
+
+	# setup.base-url: http://localhost
+        awk '
+        BEGIN { found=0 }
+        /^setup:/ { in_setup=1 }
+        in_setup && /base-url:/ { found=1; sub(/base-url:.*/, "base-url: http://localhost") }
+        { print }
+        END { if (!found) print "setup:\n  base-url: http://localhost" }
+        ' "$APP_YML" > temp.yml && mv temp.yml "$APP_YML"
+
+    else
+        echo "Skipping: $APP_YML (File not found)"
+    fi
+done
